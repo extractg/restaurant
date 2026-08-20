961bece4afdda937a66219ad2a866ad92009da08
Anton Mavrenkov <anton.mavrenkov05@gmail.com>
2026-08-20 19:14:04 +0300

    visual changes + burger menu UI

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 const backToTop = document.getElementById("backToTop");
 const burger = document.getElementById("burger");
-
+const nav = document.querySelector(".nav");
 
 window.addEventListener("scroll", function () {
-    if(window.scrollY > window.innerHeight){
-    backToTop.classList.add("is-visible");
-    }
-    else{
+    const isAtBottom =
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight - 10;
+
+    if (window.scrollY > window.innerHeight && !isAtBottom) {
+        backToTop.classList.add("is-visible");
+    } else {
         backToTop.classList.remove("is-visible");
     }
 });
-    backToTop.addEventListener("click", function () {
+
+backToTop.addEventListener("click", function () {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
-    });    
+});
+
+burger.addEventListener("click", function () {
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+})
