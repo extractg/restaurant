4ece66c3cb2f787224359f09c1c5560de602a22c
Anton Mavrenkov <anton.mavrenkov05@gmail.com>
2026-08-28 20:51:17 +0300

    favorite.js structure

--- js/favorites.js
+++ js/favorites.js
@@ -3,3 +3,19 @@ const favoritesCount = document.getElementById("favoritesCount");
 const favoritesEmpty = document.getElementById("favoritesEmpty");
 
 let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+async function getMealById(id) {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const data = await response.json();
+    return data.meals[0];
+    
+}
+
+async function getFavoriteMeals() {
+    const meals = await Promise.all(
+    favorites.map((id) => {
+    return getMealById(id);
+    })
+);
+    return meals;
+}
